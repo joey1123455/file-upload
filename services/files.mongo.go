c5973b618d08a6fc17c28b66afb69712f70d2d77
legion <jamescord40@gmail.com>
2023-12-20 10:17:08 +0100

    implemented retrieve all service

--- services/files.mongo.go
+++ services/files.mongo.go
@@ -2,25 +2,21 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/joey1123455/file-upload/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type FileDB interface {
 	SaveFile(
-		SecureURL,
-		VersionID,
-		URL,
-		PublicID,
-		FileName,
-		AssetID,
-		FileType,
-		Etag,
-		Bytes,
-		Version,
-		Signature string) (*mongo.InsertOneResult, error)
+		SecureURL, VersionID, URL, PublicID, FileName, AssetID,
+		FileType, Etag, Bytes, Version, Signature string,
+	) (*mongo.InsertOneResult, error)
+
+	RetrieveAll() ([]*models.ResorceObject, error)
 }
 
 type FileDBImp struct {
@@ -64,4 +60,23 @@ func (fdb FileDBImp) SaveFile(
 
 	res, err := fdb.collection.InsertOne(fdb.ctx, object)
 	return res, err
+}
+
+func (fdb FileDBImp) RetrieveAll() ([]*models.ResorceObject, error) {
+	files := make([]*models.ResorceObject, 0)
+	cur, err := fdb.collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		var elem *models.ResorceObject
+		err := cur.Decode(elem)
+		if err != nil {
+			return nil, err
+		}
+		files = append(files, elem)
+	}
+	fmt.Println(files)
+	return files, nil
 }
--- services/files.mongo_test.go
+++ services/files.mongo_test.go
@@ -31,3 +31,24 @@ func TestFileDB_SaveFile(t *testing.T) {
 	assert.Nil(t, err)
 	defer coll.Drop(ctx)
 }
+
+func TestFileDB_RetrieveAll(t *testing.T) {
+	ctx := context.TODO()
+	err := godotenv.Load("../app.env")
+	assert.Nil(t, err)
+	t.Log(err)
+	uri := os.Getenv("MONGO_URI")
+	mongoconn := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(ctx, mongoconn)
+	assert.Nil(t, err)
+	coll := client.Database("golang_mongodb").Collection("test")
+
+	// Create a FileDBImp
+	fdb := NewFileDB(coll, ctx)
+	// Call the SaveFile method
+	res, err := fdb.RetrieveAll()
+	// Check if the method returns the expected result or error
+	assert.NotNil(t, res)
+	assert.Nil(t, err)
+	defer coll.Drop(ctx)
+}
